c19c5d80068f3319f97a09a0cec7565490ff1e62
Darius Deah <Dariusdeah21@gmail.com>
2021-09-25 16:04:59 -0600

    adding name of users that liked user post

--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -3,6 +3,7 @@
     <div class="row">
       <div class="col-lg-12">
         <img :src="profile.coverImg" alt="">
+        <!-- TODO v if to display a grad symbol only if the user  graduted ===true -->
         <h4>graduted {{ profile.graduated }}</h4>
         <h5>{{ profile.class }}</h5>
         <p>{{ profile.bio }}</p>
@@ -11,13 +12,15 @@
             github
           </button>
         </a>
+        <!-- FIXME links back to homepage instead of linked in  -->
         <a :href="profile.linkedin">
           <button class="btn btn-primary">
             linkedin
           </button>
         </a>
-
-        <p></p>
+        <h1>{{ profile.name }}</h1>
+        <!-- TODO should only show couunt for people that arent user -->
+        <h5>followers:{{ profile.subs.length }}</h5>
       </div>
     </div>
     <Posts v-for="p in posts " :key="p.id" :post="p" />
